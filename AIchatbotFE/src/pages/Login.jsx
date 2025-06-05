2fa9a7c1e74debeaf897c376a290a78f0fbc6910
nganthuy0111 <nttngan0111@gmail.com>
2025-06-05 14:24:14 +0700

    Login Page

---
+++ AIchatbotFE/src/pages/Login.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Header from "../components/Header";
+import "./login.css";
+import { Link } from "react-router-dom";
+function Login() {
+  return (
+    <div>
+      <Header />
+      <div className="login">
+        <h1>Welcome to EduLawAI!</h1>
+        <div className="login content">
+          <div className="login email">
+            <input style={{}} type="text" placeholder="Enter your email" />
+          </div>
+          <div className="login password">
+            <input type="text" placeholder="Enter your password" />
+          </div>
+          <div className="login button">
+            <button>Login</button>
+          </div>
+        </div>
+      </div>
+      <div className="signup">
+        <span>Don't have an account? </span>
+        <Link to="/signup" className="signupText">
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
