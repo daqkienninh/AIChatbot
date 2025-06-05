33b8281c226418be4ca43386b8676a54a1659858
nganthuy0111 <nttngan0111@gmail.com>
2025-06-05 14:29:16 +0700

    change login.css to Login.css

--- AIchatbotFE/src/pages/Login.jsx
+++ AIchatbotFE/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
-import "./login.css";
+import "./Login.css";
 import { Link } from "react-router-dom";
 function Login() {
   return (
